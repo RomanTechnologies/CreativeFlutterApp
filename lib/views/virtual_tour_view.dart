65e7ee5fe4a4cb55442c4e098419a54adde865ca
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-06 02:21:26 +0000

    Add VirtualTourView UI screen

---
+++ lib/views/virtual_tour_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/flutter.dart';
+import 'package:panoramic/viewer.panoramic_viewer.dart';
+
+class VirtualTourView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PanoramicViewer(
+      // Initial settings and configuration for virtual tours.
+    );
+  }
+}
