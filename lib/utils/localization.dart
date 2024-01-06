d63391fb045d5bc4d260a63c94cf332c59773da9
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-06 02:20:32 +0000

    Add localization utility

---
+++ lib/utils/localization.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/flutter.dart';
+
+class LocalizationUtil {
+  static Locale? getDefaultLocale() {
+    // Implement logic to get the default locale based on user preferences or system settings.
+    return null;
+  }
+}
