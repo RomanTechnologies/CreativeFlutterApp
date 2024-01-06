cb8c4f4c9a38db9b7c2f639a1b543fc90d978a7c
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-06 02:16:26 +0000

    Add theme configuration file

---
+++ lib/_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/flutter.dart';
+
+final ThemeData appTheme = ThemeData(
+  primarySwatch: Colors.orange,
+  accentColor: Colors.blue,
+  visualDensity: VisualDensity.adaptive,
+  fontFamily: 'Georgia',
+);
