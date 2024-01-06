0cb2175feaba4abdd7c0c19aa73846de44edd960
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-06 02:16:12 +0000

    Add HomeView widget

---
+++ lib/views/home_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/flutter.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(\n      child: Center(\n        child: Text('Welcome to Virtual Travel Guide'),\n      ),\n    );
+  }
+}
