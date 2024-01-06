de47a1ee1088d58f30d50dedf550fa237a83f2e2
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-06 02:17:34 +0000

    Add FirebaseService for backend communication

---
+++ lib/services/firebase_service.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+
+class FirebaseService {
+  // Initialize Firebase and connect to database, auth, etc.
+  FirebaseService() {}
+}
