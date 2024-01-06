25b140a2d5abcbbc332fc994453505ca7d514d96
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-06 02:19:40 +0000

    Add LanguagePhrasebook widget

---
+++ lib/widgets/language_phrasebook.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/flutter.dart';
+
+class LanguagePhrasebook extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scrollable(\n      child: Column(\n        children: [\n          // This widget will display language phrasebook items.\n        ],\n      ),\n    );
+  }
+}
