25202c5dc6a1f6a194df47ec3a10865707aba465
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-06 23:42:13 +0000

    Add CultureCuisineView UI screen

---
+++ lib/views/culture_cuisine_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/flutter.dart';
+
+class CultureCuisineView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scrollable(\n      child: Column(\n        children: [\n          // This screen will display information on local culture and cuisine.\n        ],\n      ),\n    );
+  }
+}
