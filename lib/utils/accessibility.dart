50a4fdb2c62c3d5a300900bea1169d1590d3974d
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-06 02:21:01 +0000

    Add accessibility utility

---
+++ lib/utils/accessibility.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/flutter.dart';
+
+class AccessibilityUtil {
+  // Methods and properties to enable and improve accessibility features in the app.
+  // This could include text to speech, high contrast modes, etc.
+  static void enableHighContrastMode() {}
+}
