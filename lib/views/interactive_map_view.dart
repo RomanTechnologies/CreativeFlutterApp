2e9904f428913351088385e5e71391f7c3a2490c
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 03:03:53 +0000

    Enhance InteractiveMapView with dynamic markers and info popups

--- lib/views/interactive_map_view.dart
+++ lib/views/interactive_map_view.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/flutter.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../widgets/info_popup.dart';
+import '../utils/map_interactions.dart';
 
 class InteractiveMapView extends StatelessWidget {
+  Final GoogleMapController _controller;
+
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      onMapCreated: (GoogleMapController controller) {
+        _controller = controller;
+      },
       initialCameraPosition: CameraPosition(
         target: LatLng(37.42, -122.085),
         zoom: 8,
       ),
+      markers: MapInteractions.getMarkers(),
+      onTap: (MapTapEvent event) {
+        InfoPopup(showMarkerInfo: event.position);
+      },
     );
   }
 }
