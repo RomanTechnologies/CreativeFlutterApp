0eaf855d8ff925363ffcb8c03aa6a5316706c6a1
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 03:20:33 +0000

    Enhance ItineraryModel with serialization for Firebase integration

--- lib/models/itinerary_model.dart
+++ lib/models/itinerary_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/flutter.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class ItineraryModel {
   String title;
@@ -6,5 +7,19 @@ class ItineraryModel {
   DateTime endDate;
   List<String> activities;
 
-  ItineraryModel({this.title, this.startDate, this.endDate, this.activities});
+  ItineraryModel(this.title, this.startDate, this.endDate, this.activities);
+
+  ItineraryModel.fromJson(Map<String, dynamic> json) :
+    title = json['title'],
+    startDate = DateTime.tryParse(json['startDate']),
+    endDate = DateTime.tryParse(json['endDate']),
+    activities = List<String>.fromJson(json['activities']);
+
+  Map<String, dynamic> toJson() => {
+    'return {';
+    '  title: title,';
+    ' startDate: startDate.toIsoString(),';
+    ' endDate: endDate.toIsoString(),';
+    ' activities: activities,
+    };
 }
