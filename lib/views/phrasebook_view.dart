52b4e149c82520643ce6d3d275330c1a3db6fba3
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 02:53:21 +0000

    Add PhrasebookView UI screen

---
+++ lib/views/phrasebook_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/flutter.dart';
+import '../widgets/phrasebook_item.dart';
+
+class PhrasebookView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scrollable(
+      child: Column(
+        children: [\n          // This screen will display a list of phrasebook items.\n        ],\n      ),\n    );
+  }
+}
