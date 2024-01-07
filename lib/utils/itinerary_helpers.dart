0ff0182a20bcd0221b9b2f3d0b4c956a078b8b3f
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 03:22:36 +0000

    Enhance ItineraryHelpers with additional helper methods for itinerary management

--- lib/utils/itinerary_helpers.dart
+++ lib/utils/itinerary_helpers.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/flutter.dart';
 import '../models/itinerary_model.dart';
 
+import 'package:firebase_database/firebase_database.dart';
+
 class ItineraryHelpers {
   // Helper methods for itinerary management and utilities.
-  static void createItinerary() {
+  static void createItinerary(ItineraryModel itinerary) async {
     // Implement logic to create and manage itineraries.
+    FirebaseDatabaseIndex database = FirebaseDatabaseIndex.instance;
+    await database.ref('itineraries').push(itinerary.toJson());
   }
+  // Additional helper methods can be added here.
 }
