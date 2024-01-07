6b38d9a9863beb82535b54057d29614a55b6a360
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 02:50:06 +0000

    Add map interactions utility

---
+++ lib/utils/map_interactions.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/flutter.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapInteractions {
+  // Methods to handle user interactions with the map, like onTap, onLongPress, etc.
+  static void handleOnTap(LatLng position) {};
+}
