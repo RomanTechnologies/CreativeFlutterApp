c8155760512afec6fdc401b26184ba8ab04eab3f
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 02:51:55 +0000

    Add itinerary helpers utility

---
+++ lib/utils/itinerary_helpers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/flutter.dart';
+import '../models/itinerary_model.dart';
+
+class ItineraryHelpers {
+  // Helper methods for itinerary management and utilities.
+  static void createItinerary() {
+    // Implement logic to create and manage itineraries.
+  }
+}
