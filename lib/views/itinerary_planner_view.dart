01493eeaf03493f940b43dff89c2182f5b5fe474
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 02:49:04 +0000

    Add ItineraryPlannerView UI screen

---
+++ lib/views/itinerary_planner_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/flutter.dart';
+
+class ItineraryPlannerView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scrollable(
+      child: Column(
+        children: [\n          // This screen will allow users to plan their travel itineraries.\n        ],\n      ),
+    );
+  }
+}
