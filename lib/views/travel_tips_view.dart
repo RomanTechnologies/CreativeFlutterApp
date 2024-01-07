a826585f51266d17c84c89f750acfb88e72369f7
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 02:54:49 +0000

    Add TravelTipsView UI screen

---
+++ lib/views/travel_tips_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/flutter.dart';
+
+class TravelTipsView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scrollable(
+      child: Column(
+        children: [\n          // This screen will display travel tips and advice.\n        ],\n      ),\n    );
+  }
+}
