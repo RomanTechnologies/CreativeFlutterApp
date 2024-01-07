1247be1a48e9b484eed04960a7d6811c33b0c8e8
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 02:54:20 +0000

    Add phrasebook helpers utility

---
+++ lib/utils/phrasebook_helpers.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/flutter.dart';
+import '../models/itinerary_model.dart';
+
+class PhrasebookHelpers {
+  // Helper methods for phrasebook management and utilities.
+  static void createPhrasebook() {
+    // Implement logic to create and manage phrasebooks.
+  }
+}
