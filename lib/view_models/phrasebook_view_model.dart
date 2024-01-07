dd36309995f6083baa5a5bfbc813cc2c84914c52
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 03:28:40 +0000

    Enhance PhrasebookViewModel with advanced data handling and notifications

--- lib/view_models/phrasebook_view_model.dart
+++ lib/view_models/phrasebook_view_model.dart
@@ -3,6 +3,8 @@ import '../models/itinerary_model.dart';
 
 import '../services/firebase_service.dart';
 
+import 'package:firebase_database/firebase_database.dart';
+
 class PhrasebookViewModel {
   // Methods and data for phrasebook logic and interaction.
   void loadPhrasebook() {}
