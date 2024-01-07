3faca263e1ee29abada8df16ca1faddc1a55387b
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 03:00:18 +0000

    Enhance TravelTipsView with dynamic content

--- lib/views/travel_tips_view.dart
+++ lib/views/travel_tips_view.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/flutter.dart';
+import '../widgets/travel_tip_item.dart';
+import '../view_models/travel_tips_view_model.dart';
 
 class TravelTipsView extends StatelessWidget {
+  final TravelTipsViewModel viewModel;
+
+  TravelTipsView(this.viewModel);
+
   @override
   Widget build(BuildContext context) {
-    return Scrollable(
-      child: Column(
-        children: [\n          // This screen will display travel tips and advice.\n        ],\n      ),\n    );
+    return Scaffold(
+      appBar: AppBar(title: Text('Travel Tips')),
+      body: FutureBuilder<List<TravelTipItem>>(
+        future: viewModel.getTravelTips(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (index) {
+                return TravelTipItem(title: snapshot.data[index].title, description: snapshot.data[index].description);
+              },
+          );
+        },
+      ),
+    );
   }
 }
