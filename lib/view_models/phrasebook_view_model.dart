35b69a14bdf97b05f449311894694de6accfaadb
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 02:51:33 +0000

    Add PhrasebookViewModel for phrasebook logic

---
+++ lib/view_models/phrasebook_view_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/flutter.dart';
+import '../models/itinerary_model.dart';
+
+import '../services/firebase_service.dart';
+
+class PhrasebookViewModel {
+  // Methods and data for phrasebook logic and interaction.
+  void loadPhrasebook() {}
+}
