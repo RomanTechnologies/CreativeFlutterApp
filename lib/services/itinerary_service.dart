6b927e2ea439bdc21fdcb909757bf9963aaf0087
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 02:51:12 +0000

    Add ItineraryService for itinerary management

---
+++ lib/services/itinerary_service.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/flutter.dart';
+import '../models/itinerary_model.dart';
+
+import '../services/firebase_service.dart';
+
+class ItineraryService {
+  // Methods for itinerary creation, updating, and retrieval.
+  void addItinerary(ItineraryModel itinerary) {}
+}
