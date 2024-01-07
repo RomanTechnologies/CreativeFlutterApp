305bbdf4e812953c71b7d697095fbbbcdeb37a43
Peter Williams <34144944+RomanTechnologies@users.noreply.github.com>
2024-01-07 03:15:41 +0000

    Enhance MapInteractions with advanced interaction features

--- lib/utils/map_interactions.dart
+++ lib/utils/map_interactions.dart
@@ -3,5 +3,11 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapInteractions {
   // Methods to handle user interactions with the map, like onTap, onLongPress, etc.
-  static void handleOnTap(LatLng position) {};
+  static void handleOnTap(LatLng position) {
+    // Advanced handling of map taps for more engaging interactions.
+  }
+
+  static void handleOnLongPress(LatLng position) {
+    // Implement handling of long presses for additional map functionalities.
+  }
 }
